a55430e2c3617bb3f3bd9ee11cbc3fa7c6fe0c37
haideralwaqfi <haideralwaqfi@gmail.com>
2022-07-16 00:12:37 +0300

    ep3 design new idea

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans bg-gary-background antialiased">
 
@@ -49,8 +50,38 @@
         </header>
 <main class="container mx-auto max-w-custom flex">
     <div class="w-70 mr-5" >
-        Ad idea goes here    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis tempora facere rerum laboriosam aut nesciunt laborum recusandae deserunt voluptates, tenetur nihil officia eum nam dolore corrupti. Iusto eaque perspiciatis, tenetur laborum tempore ex provident aut! Odit corporis illum porro quas, consectetur ducimus enim nisi eum. Ullam doloremque cupiditate cum distinctio impedit. Magni quisquam quia soluta aut at corrupti doloremque harum. Harum odio commodi esse ex, cupiditate reprehenderit quo culpa voluptas provident repellendus dolorem fugiat cum vel laboriosam amet earum perspiciatis, saepe quod beatae sit ab qui ipsa. Rem qui saepe deserunt nam deleniti vel numquam ipsum animi repellendus, quos provident?
+        <div class="border-2 border-blue-100 rounded-xl mt-16 bg-white">
+            <div class="text-center px-6 py-2 pt-6">
+                <h3 class="font-semi-bold">Add new Idea</h3>
+                <p class="text-xs mt-4 text-center">Let us new what you wuld like to and we will take a look</p>
+            </div>
+            <form class="space-y-4 px-4 py-6" action="#" method="POST">
+                <input id="idea" type="text" placeholder="Enter Your Idea" class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 px-4 py-2 border-none">
+                <div class="">
+                    <select name="category_add" id="category_add" class="bg-gray-100 text-sm w-full border-none rounded-xl px-4 py-2">
+                        <option value="Category One">Category One</option>
+                        <option value="Category Two">Category Two</option>
+                        <option value="Category Three">Category Three</option>
+                        <option value="Category Four">Category Four</option>
+                    </select>
+                </div>
+                <div>
+                    <textarea name="idea" id="idea" cols="30" rows="4" class="bg-gray-100 text-sm rounded-xl border-none w-full placeholder-gray-900 " placeholder="Describe Your Idea"></textarea>
+                </div>
+                <div class="flex item-center justify-between space-x-3">
+                    <button type="button" class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                        <svg class="text-gray-500 w-5 transform -rotate-45" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                          </svg>
+                        <span class="">Attach</span></button>
+                        <button type="submit" class="lex items-center justify-center w-1/2 h-11 text-xs bg-blue-400 font-semibold rounded-xl border border-blue-200 text-white hover:border-blue-600 transition duration-150 ease-in px-6 py-3">
+                            <span class="">Submit</span></button>
 
+                </div>
+
+
+            </form>
+        </div>
     </div>
     <div class="w-175">
         <nav class="flex items-center jusstify-between text-sm">
