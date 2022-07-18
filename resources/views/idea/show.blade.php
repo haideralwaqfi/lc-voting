e2a4cc0c9ab95db33ab0e03f36cb7b671f059c0b
haideralwaqfi <haideralwaqfi@gmail.com>
2022-07-18 20:47:42 +0300

    ep12

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -11,9 +11,8 @@
     <div class="idea-container bg-white rounded-xl flex mt-4">
         <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
             <div class="flex-none mx-2 md:mx-4">
-                <a href="#">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
-                </a>
+                <img src="{{$idea->user->getAvatar()}}" alt="avatar"
+                class="w-14 h-14 rounded-xl">
             </div>
             <div class="w-full mx-2 md:mx-4">
                 <h4 class="text-xl font-semibold">
