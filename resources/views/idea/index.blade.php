247240c5c12bc8203070e2ff4be65af4d0a3df15
haideralwaqfi <haideralwaqfi@gmail.com>
2022-07-19 23:52:20 +0300

    fix tailwind issue

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -30,8 +30,7 @@
 
     <div class="ideas-container space-y-6 my-8">
         @foreach ($ideas as $idea)
-            <div
-                x-data
+            <div x-data
                 @click="
                     const clicked = $event.target
                     target = clicked.tagName.toLowerCase()
@@ -56,21 +55,21 @@
                 <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
                     <div class="flex-none mx-2 md:mx-0">
                         <a href="#">
-                            <img src="{{$idea->user->getAvatar()}}" alt="avatar"
-                                class="w-14 h-14 rounded-xl">
+                            <img src="{{ $idea->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl">
                         </a>
                     </div>
                     <div class="w-full flex flex-col justify-between mx-2 md:mx-4">
                         <h4 class="text-xl font-semibold mt-2 md:mt-0">
-                            <a href="{{ route('idea.show', $idea) }}" class="idea-link hover:underline">{{$idea->title}}</a>
+                            <a href="{{ route('idea.show', $idea) }}"
+                                class="idea-link hover:underline">{{ $idea->title }}</a>
                         </h4>
                         <div class="text-gray-600 mt-3 line-clamp-3">
-                            {{$idea->description}}
+                            {{ $idea->description }}
                         </div>
 
                         <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
                             <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                                <div>{{$idea->created_at->diffForHumans()}}</div>
+                                <div>{{ $idea->created_at->diffForHumans() }}</div>
                                 <div>&bull;</div>
                                 <div>{{ $idea->category->name }}</div>
                                 <div>&bull;</div>
@@ -87,8 +86,8 @@
                                             d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
                                             style="color: rgba(163, 163, 163, .5)">
                                     </svg>
-                                    <ul x-cloak x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false"
-                                        @keydown.escape.window="isOpen = false"
+                                    <ul x-cloak x-show.origin.top.left="isOpen" x-transition
+                                        @click.away="isOpen = false" @keydown.escape.window="isOpen = false"
                                         class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0">
                                         <li><a href="#"
                                                 class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark
