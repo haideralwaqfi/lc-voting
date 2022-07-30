a2ad163b3d72eb8983c29934fad0f7d20f0ec931
haideralwaqfi <haideralwaqfi@gmail.com>
2022-07-30 14:07:16 +0300

    ep39

--- resources/views/livewire/edit-idea.blade.php
+++ resources/views/livewire/edit-idea.blade.php
@@ -1,44 +1,35 @@
 <div
     x-cloak
-    x-data="{isOpen: false}"
+    x-data="{ isOpen: false }"
     x-show="isOpen"
     @keydown.escape.window="isOpen = false"
     @custom-show-edit-modal.window="isOpen = true"
-class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div
-    x-show.opacity="isOpen"
-    x-transition
-    class="flex items-end justify-center min-h-screen">
-        <!--
-      Background overlay, show/hide based on modal state.
-
-      Entering: "ease-out duration-300"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "ease-in duration-200"
-        From: "opacity-100"
-        To: "opacity-0"
-    -->
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-
-        <!--
-      Modal panel, show/hide based on modal state.
-
-      Entering: "ease-out duration-300"
-        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        To: "opacity-100 translate-y-0 sm:scale-100"
-      Leaving: "ease-in duration-200"
-        From: "opacity-100 translate-y-0 sm:scale-100"
-        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-    -->
+    x-init="
+        window.livewire.on('ideaWasUpdated', () => {
+            isOpen = false
+        })
+    "
+    class="fixed z-10 inset-0 overflow-y-auto"
+    aria-labelledby="modal-title"
+    role="dialog"
+    aria-modal="true"
+>
+    <div class="flex items-end justify-center min-h-screen">
         <div
-        x-show.origin.bottom.duration.400ms="isOpen"
-        x-transition
-        class="modal bg-white rounded-tl-xl rounded-tr-xl overflow-hidden transform transition-all py-4 sm:max-w-lg sm:w-full">
+            x-show.transition.opacity="isOpen"
+            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            aria-hidden="true">
+        </div>
+
+        <div
+            x-show.transition.origin.bottom.duration.300ms="isOpen"
+            class="modal bg-white rounded-tl-xl rounded-tr-xl overflow-hidden transform transition-all py-4 sm:max-w-lg sm:w-full"
+        >
             <div class="absolute top-0 right-0 pt-4 pr-4">
                 <button
-                @click="isOpen=false"
-                class="text-gray-400 hover:text-gray-500">
+                    @click="isOpen = false"
+                    class="text-gray-400 hover:text-gray-500"
+                >
                     <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
@@ -49,7 +40,7 @@ class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="d
                 <h3 class="text-center text-lg font-medium text-gray-900">Edit Idea</h3>
                 <p class="text-xs text-center leading-5 text-gray-500 px-6 mt-4">You have one hour to edit your idea from the time you created it.</p>
 
-                <form wire:submit.prevent="createIdea" action="#" method="POST" class="space-y-4 px-4 py-6">
+                <form wire:submit.prevent="updateIdea" action="#" method="POST" class="space-y-4 px-4 py-6">
                     <div>
                         <input wire:model.defer="title" type="text" class="w-full text-sm bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your Idea" required>
                         @error('title')
@@ -58,8 +49,9 @@ class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="d
                     </div>
                     <div>
                         <select wire:model.defer="category" name="category_add" id="category_add" class="w-full bg-gray-100 text-sm rounded-xl border-none px-4 py-2">
-
-                            <option value="1">Category 1</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                     @error('category')
@@ -83,7 +75,7 @@ class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="d
                         <button
                                 type="submit"
                                 class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3">
-                            <span class="ml-1">Submit</span>
+                            <span class="ml-1">Update</span>
                         </button>
                     </div>
                 </form>
--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -43,13 +43,16 @@
                             <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl z-10 py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0"
                                 x-cloak x-show.origin.top.left="isOpen" x-transition @click.away="isOpen = false"
                                 @keydown.escape.window="isOpen = false">
+                                @can('update', $idea)
                                 <li><a href="#"
-                                        @click="
-                                            isOpen = !isOpen
-                                            $dispatch('custom-show-edit-modal')
-                                            "
-                                        class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Edit
-                                        Idea</a></li>
+                                    @click="
+                                        isOpen = !isOpen
+                                        $dispatch('custom-show-edit-modal')
+                                        "
+                                    class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Edit
+                                    Idea</a></li>
+                                @endcan
+
                                 <li><a href="#"
                                         class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete
                                         Idea</a></li>
